c0c8a774a1a649c22e04fc70bc5ef1adb48384bd
mari2326 <pcoamo.o5@gmail.com>
2021-09-04 23:33:21 +0900

    nicknameのバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :nickname, presence: true, length: { maximum: 6 }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :cafes
+  has_many :comments
+
+
+
 end
